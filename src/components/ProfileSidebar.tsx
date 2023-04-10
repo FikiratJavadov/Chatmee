5b1e76c92048c2ea5c6b27de34177bc78ec19aec
Fikirat Javadov <iamjavadov@gmail.com>
2023-04-10 19:14:14 +0400

    fixed image display on Sidebar

--- src/components/ProfileSidebar.tsx
+++ src/components/ProfileSidebar.tsx
@@ -2,15 +2,13 @@ import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
 import Image from 'next/image';
 
-interface ProfileSidebarProps {}
-
 const ProfileSidebar = async ({}) => {
   const session = await getServerSession(authOptions);
   return (
     <>
       <div className="flex items-center gap-3">
         {session?.user?.image && (
-          <div className="h-12 w-12 rounded-lg overflow-hidden">
+          <div className=" h-12 w-12 shrink-0 rounded-lg overflow-hidden bg-red-500">
             <Image
               src={session?.user?.image}
               alt="image"
@@ -21,7 +19,7 @@ const ProfileSidebar = async ({}) => {
         )}
         <div>
           <h2 className="text-xl">{session?.user.name}</h2>
-          <h5>{session?.user.email}</h5>
+          <h5 className='text-sm'>{session?.user.email}</h5>
         </div>
       </div>
     </>
